15c7c62af79962a47e09aa2f037d31323b2d9787
Tomek Kaczanowski <tomekkaczanowski@o2.pl>
2014-05-27 20:22:18 +0000

    starting work on JSON to Asciidoctor renderer

---
+++ src/main/groovy/com/practicalunittesting/ppl/renderer/JSONToAsciidoctorRenderer.groovy
@@ -0,0 +1,4 @@
+package com.practicalunittesting.ppl.renderer
+
+class JSONToAsciidoctorRenderer {
+}
---
+++ src/test/groovy/com/practicalunittesting/ppl/renderer/JSONToAsciidoctorRendererTest.groovy
@@ -0,0 +1,10 @@
+package com.practicalunittesting.ppl.renderer
+
+import spock.lang.Specification
+
+class JSONToAsciidoctorRendererTest extends Specification {
+
+    def "should render people search result"() {
+        // TODO
+    }
+}
